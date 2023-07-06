763a5c5991de042d8afea82905d1cb889fda4853
brightkingpin <luvandabright781@gmail.com>
2023-07-06 14:50:27 +0300

    Committed assignment

--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -1,23 +1,18 @@
+import React from 'react';
 
-
-
-function Gallery(){
-    return (
-        <div>
-            <h1 className="mt-5 text-centre main heading">Pocket Booth App</h1>
-            <div className="menu-tab container">
-                <div className="menu-tab d-flex justify center-around">
-                    <btn className="button">Homepage</btn>
-                    <btn className="button">Favorite</btn>
-                    <btn className="button">Private</btn>
-                </div>
-                <div>
-                <input type="search"></input>
-                <btn>search</btn>
-                </div>
-            </div>
-
+function Gallery() {
+  return (
+    <div>
+      <h1 className="mt-5 text-center main heading">Pocket Booth App</h1>
+      <div className="menu-tab container">
+        <div className="menu-tab d-flex justify-center-around">
+          <button className="button">Homepage</button>
+          <button className="button">Favorite</button>
+          <button className="button">Private</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Gallery
+
+export default Gallery;
