872fd32f4414923f635d8c098d6bfb061fd367e4
richard miruka onsare <richardonsare15@gmail.com>
2023-07-06 20:59:43 +0300

    done with the backend

--- server/app.py
+++ server/app.py
@@ -5,11 +5,12 @@ from flask_migrate import Migrate
 from models import db, User, Image, Friend
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+
 migrate = Migrate(app, db)
+db.init_app(app)
 
 @app.route('/')
 def home():
