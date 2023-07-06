c34ef4d34d49676344665e772e81818f6cbe0c94
richard miruka onsare <richardonsare15@gmail.com>
2023-07-06 14:03:22 +0300

    created class Image

--- server/models.py
+++ server/models.py
@@ -4,16 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
  
 
-class user(db.metadatasodel):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     
 def __init__(self, name):
     self.name = name
 
-class Image(db.Model)
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(100), nullable=False)
+    
 def __init__ (self, filename=None) :
-    pass
+    self.filename = filename
 
 class Friends :
 # Class for storing friends list and their details
