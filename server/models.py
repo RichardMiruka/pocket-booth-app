6447be3a620942d6359c93b28a4d32aa889fc01b
richard miruka onsare <richardonsare15@gmail.com>
2023-07-06 14:16:45 +0300

    created class Friend

--- server/models.py
+++ server/models.py
@@ -18,6 +18,12 @@ class Image(db.Model):
 def __init__ (self, filename=None) :
     self.filename = filename
 
-class Friends :
+class Friends(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    user = db.relationship('User', foreign_keys=[user_id],backref=db.backref('friends'))
+    friend = db.relationship('User', foreign_keys=[friend_id])
 # Class for storing friends list and their details
  pass
