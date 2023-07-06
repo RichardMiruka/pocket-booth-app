ebbae0c8f3b3e481f60b54433dd561451755419b
richard miruka onsare <richardonsare15@gmail.com>
2023-07-06 14:00:05 +0300

    class User created

--- server/models.py
+++ server/models.py
@@ -4,12 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
  
 
-class user:
+class user(db.metadatasodel):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    
 def __init__(self, name):
     self.name = name
-    pass
 
-class Image:
+class Image(db.Model)
 def __init__ (self, filename=None) :
     pass
 
