ff76c2443ff445bac3fbde5f8853eb8ae288557e
brightkingpin <luvandabright781@gmail.com>
2023-07-06 23:30:46 +0300

    Committed assignment

--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -1,39 +1,39 @@
+import React from 'react';
 import './App.css';
 
-
 function Gallery() {
-    const items = []; // Assuming you have an array of items
+  const items = []; // Assuming you have an array of items
 
-    return (
-        <div>
-            <h1 className="nav-bar">Pocket Booth App</h1>
-            <div className="menu-tab container">
-                <div className="menu-tab d-flex justify-content-around">
-                    <button className="button">Homepage</button>
-                    <button className="button">Favorite</button>
-                    <button className="button">Private</button>
-                </div>
-            </div>
-            <div className="menu-items container-fluid mt-5">
-                <div className="row">
-                    <div className="col-11 mx-auto">
-                        {items.map((element) => {
-                            const { id, user, image, title } = element;
-                            return (
-                                <div key={id}>
-                                    <a href={`/item/${id}`}>{title}</a><br />
-                                    <div className="col-12 col-md-12 col-lg-4 img-div">
-                                        <img src={image} alt="photo of us" className="img-fluid" />
-                                    </div>
-                                    <p>{user}</p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <h1 className="nav-bar">Pocket Booth App</h1>
+      <div className="menu-tab container">
+        <div className="menu-tab d-flex justify-content-around">
+          <button className="button">Homepage</button>
+          <button className="button">Favorite</button>
+          <button className="button">Private</button>
         </div>
-    );
+      </div>
+      <div className="menu-items container-fluid mt-5">
+        <div className="row">
+          <div className="col-11 mx-auto">
+            {items.map((element) => {
+              const { id, user, image, title } = element;
+              return (
+                <div key={id}>
+                  <a href={`/item/${id}`}>{title}</a><br />
+                  <div className="col-12 col-md-12 col-lg-4 img-div">
+                    <img src={image} alt={title} className="img-fluid" />
+                  </div>
+                  <p>{user}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Gallery;
--- client/src/UseServer.js
+++ client/src/UseServer.js
@@ -1,1 +1,46 @@
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+
+function UseServer() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://127.0.0.1:5000');
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      {/* Display the fetched data */}
+      {data && (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default UseServer;
