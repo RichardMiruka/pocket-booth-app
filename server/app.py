10c14926fa9e59afe62beb609faa071040a7a3ec
richard miruka onsare <richardonsare15@gmail.com>
2023-07-06 15:43:03 +0300

    updated my seed.py file

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-from models import db, User,Image, Friend
+from models import db, User, Image, Friend
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
@@ -13,7 +13,98 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
-    return 'Welcome to the Pocket Booth App'
+    return 'Welcome to the Pocket Booth App!'
+
+# Route to post an image
+@app.route('/images', methods=['POST'])
+def post_image():
+    data = request.json
+    filename = data.get('filename')
+
+    if not filename:
+        return jsonify({'error': 'Filename is required'}), 400
+
+    image = Image(filename=filename)
+    db.session.add(image)
+    db.session.commit()
+
+    return jsonify({'message': 'Image posted successfully'}), 201
+
+# Route to delete an image
+@app.route('/images/<int:image_id>', methods=['DELETE'])
+def delete_image(image_id):
+    image = Image.query.get(image_id)
+
+    if not image:
+        return jsonify({'error': 'Image not found'}), 404
+
+    db.session.delete(image)
+    db.session.commit()
+
+    return jsonify({'message': 'Image deleted successfully'}), 200
+
+# Route to get all images
+@app.route('/images', methods=['GET'])
+def get_images():
+    images = Image.query.all()
+    image_list = [{'id': image.id, 'filename': image.filename} for image in images]
+
+    return jsonify(image_list), 200
+
+# Route to post a comment on an image
+@app.route('/images/<int:image_id>/comments', methods=['POST'])
+def post_comment(image_id):
+    image = Image.query.get(image_id)
+
+    if not image:
+        return jsonify({'error': 'Image not found'}), 404
+
+    data = request.json
+    comment = data.get('comment')
+
+    if not comment:
+        return jsonify({'error': 'Comment is required'}), 400
+
+    image.comments.append(comment)
+    db.session.commit()
+
+    return jsonify({'message': 'Comment posted successfully'}), 201
+
+# Route to like an image
+@app.route('/images/<int:image_id>/like', methods=['POST'])
+def like_image(image_id):
+    image = Image.query.get(image_id)
+
+    if not image:
+        return jsonify({'error': 'Image not found'}), 404
+
+    image.likes += 1
+    db.session.commit()
+
+    return jsonify({'message': 'Image liked successfully'}), 200
+
+# Route to get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [{'id': user.id, 'name': user.name} for user in users]
+
+    return jsonify(user_list), 200
+
+# Route to create a user
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    name = data.get('name')
+
+    if not name:
+        return jsonify({'error': 'Name is required'}), 400
+
+    user = User(name=name)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -3,17 +3,32 @@ from models import User, Image, Friend
 
 with app.app_context():
     db.create_all()
-    
-    #Create a User
-    user = User(name='Rebecca')
-    db.session.add(user)
+
+    # Create users
+    user1 = User(name='Richard')
+    user2 = User(name='Sharoun')
+    user3 = User(name='Rebecca')
+
+    db.session.add_all([user1, user2, user3])
     db.session.commit()
-    
-    #Create an image associated with the User
-    #and save it to disk (in this case as testimage12345678
-    image = Image(filename='Rebecca.jpg')
-    image.user = user
-    db.session.add(image)
+
+    # Create images for user1
+    image1 = Image(filename='image1.jpg')
+    image2 = Image(filename='image2.jpg')
+
+    image1.user = user1
+    image2.user = user1
+
+    db.session.add_all([image1, image2])
     db.session.commit()
+
+    # Create friends
+    friend1 = Friend(user=user1, friend=user2)
+    friend2 = Friend(user=user1, friend=user3)
+    friend3 = Friend(user=user2, friend=user3)
+
+    db.session.add_all([friend1, friend2, friend3])
+    db.session.commit()
+
     
     
