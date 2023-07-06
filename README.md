00d0d9cbcd745c84dc218d7dee0d3f2b72ba8481
[sharoun <[madoyasharoun@gmail.com]>
2023-07-06 13:07:06 +0300

    Read me shorts

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ on the nav-bar
  the homepage displays random pictures set at shuffles
  the favourites shows liked pictures by user 
  the Private has set password for the private pictures 
+ search  according to title 
 # Back -end at server 
+
+user - can post, can delete, can get, patch
+friends- can get 
+image- joining table, btwn friends and user ; comments, like, title, 
--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -4,13 +4,16 @@
 function Gallery(){
     return (
         <div>
-            <h1 className="mt-5 text-centre main heading"></h1>
+            <h1 className="mt-5 text-centre main heading">Pocket Booth App</h1>
             <div className="menu-tab container">
                 <div className="menu-tab d-flex justify center-around">
-                    <btn className="button"></btn>
-                    <btn className="button"></btn>
-                    <btn className="button"></btn>
-                    <input type="search"></input>
+                    <btn className="button">Homepage</btn>
+                    <btn className="button">Favorite</btn>
+                    <btn className="button">Private</btn>
+                </div>
+                <div>
+                <input type="search"></input>
+                <btn>search</btn>
                 </div>
             </div>
 
