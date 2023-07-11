905196e77e07362eb685307d6ca3ecd1699427ef
richard miruka onsare <richardonsare15@gmail.com>
2023-07-11 08:34:11 +0300

    corrected my file

--- server/app.py
+++ server/app.py
@@ -1,44 +1,52 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request, jsonify,make_response
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db, User, Image, Friend
 
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('images', cascade='all, delete-orphan'))
+
+    def __init__(self, filename=None):
+        self.filename = filename
 
 @app.route('/')
 def home():
     return 'Welcome to the Pocket Booth App!'
 
+# Route to post an image
 @app.route('/images', methods=['POST'])
 def post_image():
     data = request.get_json()
     filename = data.get('filename')
-    user_id = data.get('user_id')
 
-    if not filename or not user_id:
-        return jsonify({'error': 'Filename and user_id are required'}), 400
+    if not filename:
+        return jsonify({'error': 'Filename is required'}), 400
 
-    # Validate and handle file upload
-    # if 'file' not in request.files:
-    #     return jsonify({'error': 'No file provided'}), 400
-
-    # file = request.files['file']
-    # Handle the uploaded file as needed (e.g., save it, process it, etc.)
-
-    image = Image(filename=filename, user_id=user_id)
+    image = Image(filename=filename)
     db.session.add(image)
     db.session.commit()
 
     return make_response(jsonify({'message': 'Image posted successfully'}), 201)
+
 # Route to delete an image
 @app.route('/images/<int:image_id>', methods=['DELETE'])
 def delete_image(image_id):
@@ -59,61 +67,6 @@ def get_images():
     image_list = [{'id': image.id, 'filename': image.filename} for image in images]
 
     return jsonify(image_list), 200
-
-# Route to post a comment on an image
-@app.route('/images/<int:image_id>/comments', methods=['POST'])
-def post_comment(image_id):
-    image = Image.query.get(image_id)
-
-    if not image:
-        return jsonify({'error': 'Image not found'}), 404
-
-    data = request.json
-    comment = data.get('comment')
-
-    if not comment:
-        return jsonify({'error': 'Comment is required'}), 400
-
-    image.comments.append(comment)
-    db.session.commit()
-
-    return jsonify({'message': 'Comment posted successfully'}), 201
-
-# Route to like an image
-@app.route('/images/<int:image_id>/like', methods=['POST'])
-def like_image(image_id):
-    image = Image.query.get(image_id)
-
-    if not image:
-        return jsonify({'error': 'Image not found'}), 404
-
-    image.likes += 1
-    db.session.commit()
-
-    return jsonify({'message': 'Image liked successfully'}), 200 
-
-# Route to get all users
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    user_list = [{'id': user.id, 'name': user.name} for user in users]
-
-    return jsonify(user_list), 200
-
-# Route to create a user
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.json
-    name = data.get('name')
-
-    if not name:
-        return jsonify({'error': 'Name is required'}), 400
-
-    user = User(name=name)
-    db.session.add(user)
-    db.session.commit()
-
-    return jsonify({'message': 'User created successfully'}), 201
 
 if __name__ == '__main__':
     app.run()
--- server/models.py
+++ server/models.py
@@ -3,29 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    friend = db.relationship('Friend', backref='user')
+    name = db.Column(db.String(100), nullable=False)
+
     def __init__(self, name):
         self.name = name
 
 class Image(db.Model):
-    __tablename__='images'
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    filename = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    friend = db.relationship('Friend', backref="images")
+    user = db.relationship('User', backref=db.backref('images', cascade='all, delete-orphan'))
 
     def __init__(self, filename=None):
         self.filename = filename
 
 class Friend(db.Model):
-    __tablename__="friend"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    # user = db.relationship('User', foreign_keys=[user_id], backref=db.backref('friends'))
-    # friend = db.relationship('User', foreign_keys=[friend_id])
+    user = db.relationship('User', foreign_keys=[user_id], backref=db.backref('friends'))
+    friend = db.relationship('User', foreign_keys=[friend_id])
